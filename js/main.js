971e4f16fa444380fb815f56a3c1a90490cc2b8e
Zhuoyun Wei <wzyboy@wzyboy.org>
2021-04-25 15:09:39 -0700

    Highlight current version in version view

--- js/main.js
+++ js/main.js
@@ -137,6 +137,7 @@ let vue = Vue.createApp({
 
             this.tabs.push({
                 name: `from: ${version.name}`,
+                versionNumber: versionNumber,
                 playing: false,
                 mapData: version.mapData,
                 fromVersion: version.version,
@@ -159,6 +160,9 @@ let vue = Vue.createApp({
         subVersions() {
             // return [];
             return this.versions.filter(version => version.fromVersion !== null);
+        },
+        currentTabObject() {
+            return this.tabs[this.currentTab];
         }
     },
     mounted() {
@@ -167,6 +171,7 @@ let vue = Vue.createApp({
 
             this.makeNewVersion();
             this.tabs[0].name = 'initial v0'
+            this.tabs[0].versionNumber = 0;
             this.tabs[0].fromVersion = 0;
         });
         emitter.on('map-data', response => {
@@ -183,6 +188,7 @@ vue.component('version-tree', {
     emits: ['new-tab'],
     props: [
         'currentVersion',
+        'currentTabObject',
         'versions',
     ],
     computed: {
